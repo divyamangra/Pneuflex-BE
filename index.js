b7671edf13c27f0b7a754a4d8c6c764693565fe0
Divyam Angra <divyam.ey@tataaig.com>
2024-03-01 14:12:58 +0530

    Changes for BE and DB connected

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI).then((res)=>{console.log("connected")}).catch((err)=>{console.log(err)})
+const app = express()
+app.listen(3000, ()=>{
+    console.log('serving at port 3000')
+})
